116c1dfcf2cda2ca02cd02557859a66710c8c374
Shadow-we <rakhaaqilahvicrie@gmail.com>
2025-10-04 13:51:28 +0700

    kalkulator

---
+++ index.js
@@ -0,0 +1,25 @@
+// dikte 
+// a = 10
+// b = 20
+
+// tambah
+const tambah = (a,b) => a+b;
+console.log(tambah(10, 20))
+
+
+// kurang
+const kurang = (a,b) => a-b;
+console.log(kurang(10, 20))
+
+
+// kali
+const kali = (a,b) => a*b;
+console.log(kali(10, 20))
+
+// bagi
+const bagi = (a,b) => a/b;
+console.log(bagi(10, 20))
+
+// modulus
+const modulus = (a,b) => a%b;
+console.log(modulus(10, 20))
