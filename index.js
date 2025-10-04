e2fbfb5cefab8106db4db5e0b985f50261a45882
Shadow-we <rakhaaqilahvicrie@gmail.com>
2025-10-04 14:03:12 +0700

    kalkulator

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-// dikte 
-// a = 10
-// b = 20
+console.log("a = 10")
+console.log("b = 20")
+console.log("----------")
 
 // tambah
 const tambah = (a,b) => a+b;
-console.log(tambah(10, 20))
-
+console.log("tambah(a, b)")
+console.log("perjumlahan 10 + 20 =",tambah(10, 20))
+console.log("----------")
 
 // kurang
 const kurang = (a,b) => a-b;
-console.log(kurang(10, 20))
-
+console.log("kurang(a, b)")
+console.log("perkurangan 10 + 20 =",kurang(10, 20))
+console.log("----------")
 
 // kali
 const kali = (a,b) => a*b;
-console.log(kali(10, 20))
+console.log("kali(a, b)")
+console.log("perkalian 10 + 20 =",kali(10, 20))
+console.log("----------")
 
 // bagi
 const bagi = (a,b) => a/b;
-console.log(bagi(10, 20))
+console.log("bagi(a, b)")
+console.log("pembagian 10 + 20 =",bagi(10, 20))
+console.log("----------")
 
 // modulus
 const modulus = (a,b) => a%b;
-console.log(modulus(10, 20))
+console.log("modulus(a, b)")
+console.log("modulus 10 + 20 =",modulus(10, 20))
