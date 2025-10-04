94d6c824bd3ea075c45f97d783910a16340f0ca7
Shadow-we <rakhaaqilahvicrie@gmail.com>
2025-10-04 14:15:50 +0700

    kalkulator

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-console.log("a = 10")
-console.log("b = 20")
+let a = 10; //ES6
+let b = 20; 
 console.log("----------")
 
 // tambah
-const tambah = (a,b) => a+b;
+const tambah = (a,b) => a+b;  //INI ARROW FUNGSI
 console.log("tambah(a, b)")
-console.log("perjumlahan 10 + 20 =",tambah(10, 20))
+console.log("perjumlahan 10 + 20 =",tambah(a, b))
 console.log("----------")
 
 // kurang
 const kurang = (a,b) => a-b;
 console.log("kurang(a, b)")
-console.log("perkurangan 10 + 20 =",kurang(10, 20))
+console.log("perkurangan 10 + 20 =",kurang(a, b))
 console.log("----------")
 
 // kali
 const kali = (a,b) => a*b;
 console.log("kali(a, b)")
-console.log("perkalian 10 + 20 =",kali(10, 20))
+console.log("perkalian 10 + 20 =",kali(a, b))
 console.log("----------")
 
 // bagi
 const bagi = (a,b) => a/b;
 console.log("bagi(a, b)")
-console.log("pembagian 10 + 20 =",bagi(10, 20))
+console.log("pembagian 10 + 20 =",bagi(a, b))
 console.log("----------")
 
 // modulus
 const modulus = (a,b) => a%b;
 console.log("modulus(a, b)")
-console.log("modulus 10 + 20 =",modulus(10, 20))
+console.log("modulus 10 + 20 =",modulus(a, b))
